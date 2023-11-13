383e8041c1f719ba2ee7f6bed14a559f128513c8
Alaeddine ZAYEN <alaedine.zayen@gmail.com>
2023-11-13 01:23:44 +0100

    Blinking led

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::gpio::*;
+use esp_idf_hal::peripherals::Peripherals;
+
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
@@ -7,4 +11,18 @@ fn main() {
     esp_idf_svc::log::EspLogger::initialize_default();
 
     log::info!("Hello, world!");
+    log::info!("Alaeddine ZAYEN!");
+
+    let peripherals = Peripherals::take().unwrap();
+    let mut led =
+        PinDriver::output(peripherals.pins.gpio2).expect("Error: Unable to set gpio2 as an Output");
+
+    loop {
+        log::info!("Setting gpio2 to 1!");
+        led.set_high().expect("Error: Unable to set gpio2 to high");
+        FreeRtos::delay_ms(1000);
+        log::info!("Setting gpio2 to 0 !");
+        led.set_low().expect("Error: Unable to set gpio2 to low");
+        FreeRtos::delay_ms(1000);
+    }
 }
